f39e3ff952053c0e55ce3beda803284a7dd146b6
Thomas Temple <tjt_84@hotmail.com>
2021-07-17 19:06:27 +1000

    added card generator

--- db/schema.rb
+++ db/schema.rb
@@ -35,5 +35,4 @@ ActiveRecord::Schema.define(version: 2021_07_17_044610) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
-
 end
